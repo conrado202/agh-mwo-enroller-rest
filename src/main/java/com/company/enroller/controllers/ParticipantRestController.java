77aa6b0ff91903ea1d9e871eb56240eb1b572061
KonradB <konradbukowicz@MacBook-Pro-Konrad.local>
2021-05-14 21:15:46 +0200

    Added Participant Service delete and add participants

--- src/main/java/com/company/enroller/controllers/ParticipantRestController.java
+++ src/main/java/com/company/enroller/controllers/ParticipantRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,5 +48,28 @@ public class ParticipantRestController {
 
 	return new ResponseEntity<Participant>(participant, HttpStatus.OK); 
 	}
+	
+	
+	public ResponseEntity<?> registerParticipant(@RequestBody Participant participant) {
+		Participant foundParticipant = participantService.findByLogin(participant.getLogin());
+		if (foundParticipant != null) {
+			return new ResponseEntity("Unable to create. A participant with login " + participant.getLogin() + " already exist.", HttpStatus.CONFLICT);
+
+		}
+		participantService.add(participant);
+		return new ResponseEntity<>(participant, HttpStatus.CREATED);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+
+	public ResponseEntity<?> delete(@PathVariable("id") String login) {
+		Participant participant = participantService.findByLogin(login);
+		if (participant == null) {
+			return new ResponseEntity(HttpStatus.NOT_FOUND);
+		}
+		participantService.delete(participant);
+		return new ResponseEntity<>(participant, HttpStatus.OK);
+	}
+	 
 
 }
