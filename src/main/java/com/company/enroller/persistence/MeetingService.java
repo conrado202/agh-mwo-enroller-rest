30ca199808dd6134a5235ab53c4d72c3490e809d
KonradB <konradbukowicz@MacBook-Pro-Konrad.local>
2021-05-14 21:48:46 +0200

    Updated Meetings Rest Controller and Meeting Service

--- src/main/java/com/company/enroller/persistence/MeetingService.java
+++ src/main/java/com/company/enroller/persistence/MeetingService.java
@@ -3,24 +3,65 @@ package com.company.enroller.persistence;
 import java.util.Collection;
 
 import org.hibernate.Query;
+import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.springframework.stereotype.Component;
-
 import com.company.enroller.model.Meeting;
+import com.company.enroller.model.Participant;
+
 
 @Component("meetingService")
 public class MeetingService {
 
-	DatabaseConnector connector;
-
+//	DatabaseConnector connector;
+	Session session;
+	
+	
 	public MeetingService() {
-		connector = DatabaseConnector.getInstance();
+//		connector = DatabaseConnector.getInstance();
+		session = DatabaseConnector.getInstance().getSession();
 	}
 
 	public Collection<Meeting> getAll() {
 		String hql = "FROM Meeting";
-		Query query = connector.getSession().createQuery(hql);
+//		Query query = connector.getSession().createQuery(hql);
+		Query query = this.session.createQuery(hql);
 		return query.list();
 	}
+
+
+	public Meeting findMeetingById(int id) {
+		Meeting meeting = (Meeting) this.session.get(Meeting.class, id);
+		return meeting;
+	}
+	
+	public void addMeeting(Meeting meeting) {
+		Transaction transaction = this.session.beginTransaction();
+		this.session.save(meeting);
+		transaction.commit();
+		
+	}
+
+	public void deleteMeeting(Meeting meeting) {
+		Transaction transaction = this.session.beginTransaction();
+		this.session.delete(meeting);
+		transaction.commit();
+		
+	}
+
+	public boolean existingMeeting(Meeting meeting) {
+		return true;
+	}
+
+	public void updateMeeting(Meeting meeting) {
+		Transaction transaction = this.session.beginTransaction();
+		this.session.merge(meeting);
+		transaction.commit();
+		
+	}
+
+
+
+
 
 }
